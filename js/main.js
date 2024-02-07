3d0487e1850e547589f85121628cbc54e6039b4b
Toukir <to.toukir.rahman@gmail.com>
2024-02-07 21:04:21 +0600

    no message

--- js/main.js
+++ js/main.js
@@ -1,47 +1,33 @@
 (function ($) {
-"use strict";
-    
-// sticky
-$(window).on('scroll', function () {
-	var scroll = $(window).scrollTop();
-	if (scroll < 50) {
-		$("#header-sticky").removeClass("sticky-menu");
-	} else {
-		$("#header-sticky").addClass("sticky-menu");
-	}
-});
+	"use strict";
 
-
-// smoth scroll
-$(function () {
-	$('a.smoth-scroll').on('click', function (event) {
-		var $anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top - 100
-		}, 1000);
-		event.preventDefault();
+	// sticky
+	$(window).on('scroll', function () {
+		var scroll = $(window).scrollTop();
+		if (scroll < 50) {
+			$("#header-sticky").removeClass("sticky-menu");
+		} else {
+			$("#header-sticky").addClass("sticky-menu");
+		}
 	});
-});
 
-
-// counterUp
-$('.count').counterUp({
-	delay: 100,
-	time: 1000
-});
-
-
-// scrollToTop
-$.scrollUp({
-	scrollName: 'scrollUp',
-	topDistance: '300',
-	topSpeed: 300,
-	animation: 'fade',
-	animationInSpeed: 200,
-	animationOutSpeed: 200,
-	scrollText: '<i class="fas fa-level-up-alt"></i>',
-	activeOverlay: false,
-});
-
+	// smoth scroll
+	$(function () {
+		$('.smoth-scroll').on('click', function (event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top - 0
+			}, 1500);
+			event.preventDefault();
+		});
+		$(window).on('scroll', 
+		function(){
+			if ($(this).scrollTop() === 0) {
+				$('.smoth-scroll').hide();
+			} else {
+				$('.smoth-scroll').show();
+			}
+		});
+	});
 
 })(jQuery);
